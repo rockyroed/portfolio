d250fbf7b6141a7b7dece0473b4ed21036a70caa
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-04 21:55:24 +0800

    Create Hero

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,19 @@
+import { Lines } from "./Lines";
+
+const Hero = () => {
+  return (
+    <div className="mt-[80px] flex text-center flex-col justify-center items-center">
+      <h1 className="text-5xl/16 font-bold w-1/2">
+        Paving the Web to Create Smooth Digital Journeys
+      </h1>
+      <Lines />
+      <p className="w-1/2 mt-[16px]">
+        I develop websites with a focus on functionality, responsiveness,
+        and user experience, navigating the challenges of web development to
+        create smoother digital experiences.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
