083af4b150ed7fd81e6edae7bf4984f36d3991dc
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-04 21:55:11 +0800

    Update NavBar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,14 +2,15 @@ import Button from "@/components/Button";
 
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-center px-[32px] h-[80px] text-text">
-      <div className="flex justify-left items-center w-full gap-[8px]">
+    <div className="flex items-center justify-center px-[32px] h-[80px]">
+      <div className="flex justify-left items-center w-full gap-2">
         <img src="/src/assets/formal-picture.png" alt="profile" className="w-[40px] h-[40px] rounded-full" />
-        <p className="font-semibold">CHRISTIAN ROED BOYLES</p>
+        <p className="font-semibold text-[20px]">Christian Roed Boyles</p>
       </div>
-      <div className="flex justify-right items-center gap-[8px]">
+      <div className="flex justify-right items-center gap-2">
         <Button value="About"/>
         <Button value="Projects"/>
+        <Button value="Certificates"/>
         <Button value="Contact"/>
       </div>
     </div>
