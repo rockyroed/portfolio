d3c22f6d5e4401fdb940d52c89a538c3274ff096
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-04 21:56:22 +0800

    Add NavBar and Hero to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import NavBar from "@/components/NavBar";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div className="flex flex-col">
-    <NavBar />
-  </div>;
+  return (
+    <div className="flex flex-col justify-center bg-background text-text">
+      <NavBar />
+      <Hero />
+    </div>
+  );
 };
 
 export default App;
