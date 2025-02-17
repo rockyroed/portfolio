977beafdcb5ca3078670ea80b6405e1b3aeddbfe
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-17 18:41:12 +0800

    Add about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 const App = () => {
   return (
     <div className="flex flex-col justify-center bg-background text-text mx-[16px]">
       <NavBar />
       <Hero />
+      <About />
     </div>
   );
 };
