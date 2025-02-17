fa187addeb27da9b2b8139d17c2a8aebad84960b
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-17 20:11:44 +0800

    Initial about section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,7 +1,27 @@
 const About = () => {
   return (
-    <div>
-      About
+    <div className="flex flex-col items-center h-auto gap-y-[40px] my-[80px] w-full">
+      <h2 className="font-bold text-3xl">About</h2>
+      <div className="flex gap-x-[80px] items-center w-2/3">
+        <img
+          src="/images/formal-picture.png"
+          alt="formal picture"
+          className="h-[320px] rounded-lg"
+        />
+        <p>
+          I am Christian Roed Boyles. I am a skilled Frontend Developer, Web
+          Developer, and Programmer with expertise in HTML, CSS, JavaScript, and
+          modern frameworks like React, Next.js, and Vue.js. Proficient in tools
+          such as Git, GitHub, GitLab, Python, PHP, Selenium, Bitbucket,
+          Atlassian, Jira, Azure, and AWS, I specialize in responsive web
+          design, mobile-first development, and cross-browser compatibility.
+          They excel in API integration, CI/CD, and web application security
+          while focusing on scalable solutions and server-side rendering for
+          improved SEO. With a proactive, self-motivated work ethic, I
+          consistently update my skills to deliver cutting-edge digital
+          solutions that align with business goals.
+        </p>
+      </div>
     </div>
   );
 };
