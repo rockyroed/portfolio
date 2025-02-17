feb96513b7217ed415a8c858bae6b1f8e731db97
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-17 18:03:25 +0800

    CLS fixes

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,11 +2,10 @@ import { Lines } from "./Lines";
 
 const Hero = () => {
   return (
-    <div className="mt-[80px] flex text-center flex-col justify-center items-center">
+    <div className="pt-[160px] flex text-center flex-col justify-center items-center">
       <h1 className="text-5xl/16 font-bold w-1/2">
         Paving the Web to Create Smooth Digital Journeys
       </h1>
-      <Lines />
       <p className="w-1/2 mt-[16px]">
         I develop websites with a focus on functionality, responsiveness,
         and user experience, navigating the challenges of web development to
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,16 +2,22 @@ import Button from "@/components/Button";
 
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-center px-[32px] h-[80px]">
-      <div className="flex justify-left items-center w-full gap-2">
-        <img src="/logo-transparent.png" alt="profile" className="w-[40px] h-[40px] rounded-full" />
+    <div className="flex items-center justify-between px-[32px] h-[80px] w-full fixed top-0 bg-background shadow-md">
+      <div className="flex justify-start items-center w-full gap-2">
+        <img
+          src="/images/logo-transparent.png"
+          alt="profile"
+          className="w-[40px] h-[40px] rounded-full"
+          loading="eager"
+          decoding="async"
+        />
         <p className="font-semibold text-[20px]">rockyroed</p>
       </div>
-      <div className="flex justify-right items-center gap-2">
-        <Button value="About"/>
-        <Button value="Projects"/>
-        <Button value="Certificates"/>
-        <Button value="Contact"/>
+      <div className="flex justify-end items-center gap-2">
+        <Button value="About" />
+        <Button value="Projects" />
+        <Button value="Certificates" />
+        <Button value="Contact" />
       </div>
     </div>
   );
