8f9b63c2193f2cf2caa99c2b5b1f65a762543744
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-17 17:29:02 +0800

    Change navbar image

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className="flex items-center justify-center px-[32px] h-[80px]">
       <div className="flex justify-left items-center w-full gap-2">
-        <img src="/formal-picture.png" alt="profile" className="w-[40px] h-[40px] rounded-full" />
+        <img src="/logo-transparent.png" alt="profile" className="w-[40px] h-[40px] rounded-full" />
         <p className="font-semibold text-[20px]">rockyroed</p>
       </div>
       <div className="flex justify-right items-center gap-2">
