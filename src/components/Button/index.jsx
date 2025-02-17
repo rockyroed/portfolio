0b9784d8821798fbce63cadc45ec69886524242f
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-17 17:29:18 +0800

    Use tailwind colors

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
 const Button = ({ value }) => {
   return (
-    <button className="cursor-pointer font-normal text-text p-[8px] w-max transform transition-transform duration-200 hover:scale-110 hover:bg-radial hover:from-[#FFFFFF22] hover:to-[#11111b] rounded-full">
+    <button className="cursor-pointer font-normal text-text p-[8px] w-max transform transition-transform duration-200 hover:scale-110 hover:bg-radial hover:from-white-22 hover:to-background rounded-full">
       {value}
     </button>
   );
