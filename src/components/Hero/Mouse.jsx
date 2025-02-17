73a0b4fafa2c9b6592a373ee511f476985948e25
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-17 18:37:42 +0800

    Add mouse component

---
+++ src/components/Hero/Mouse.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export const Mouse = () => {
+  const [position, setPosition] = useState(false);
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPosition((prev) => !prev);
+    }, 1200);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="w-[24px] h-[40px] border-2 rounded-full flex justify-center items-start relative overflow-hidden">
+      <div
+        className={`w-[6px] h-[6px] bg-text rounded-full absolute top-2 transition-transform duration-[1200ms] ease-in-out ${position ? 'translate-y-[10px]' : 'translate-y-0'}`}
+      />
+    </div>
+  );
+};
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,16 +1,17 @@
-import { Lines } from "./Lines";
+import { Mouse } from "./Mouse";
 
 const Hero = () => {
   return (
-    <div className="pt-[160px] flex text-center flex-col justify-center items-center">
-      <h1 className="text-5xl/16 font-bold w-1/2">
+    <div className="pt-[160px] flex text-center flex-col justify-center items-center gap-y-[32px]">
+      <h1 className="text-5xl/16 font-bold w-2/3">
         Paving the Web to Create Smooth Digital Journeys
       </h1>
-      <p className="w-1/2 mt-[16px]">
+      <p className="w-1/2">
         I develop websites with a focus on functionality, responsiveness,
         and user experience, navigating the challenges of web development to
         create smoother digital experiences.
       </p>
+      <Mouse />
     </div>
   );
 };
