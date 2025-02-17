b8cc15a2db1089d3ff88c00d8bc16896cc47dd29
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-17 12:35:21 +0800

    Change name in navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div className="flex items-center justify-center px-[32px] h-[80px]">
       <div className="flex justify-left items-center w-full gap-2">
         <img src="/formal-picture.png" alt="profile" className="w-[40px] h-[40px] rounded-full" />
-        <p className="font-semibold text-[20px]">Christian Roed Boyles</p>
+        <p className="font-semibold text-[20px]">rockyroed</p>
       </div>
       <div className="flex justify-right items-center gap-2">
         <Button value="About"/>
