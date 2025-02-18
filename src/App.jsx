7e48b79719f328807cc9eeb7a9caabfff2ba5d9e
rockyroed <roed-imp.vo97b@slmail.me>
2025-02-18 11:21:44 +0800

    Add stats section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Stats from "@/components/Stats";
 
 const App = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-background text-text mx-[16px]">
+    <div className="flex flex-col justify-center items-center text-foreground mx-[16px]">
       <div className="w-full">
         <NavBar />
         <Hero />
       </div>
       <About />
+      <Stats />
     </div>
   );
 };
