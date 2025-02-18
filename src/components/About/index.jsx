1e264ad8dba8f633fd5718cf483ba311db62b22c
rockyroed <roed-imp.vo97b@slmail.me>
2025-02-18 09:56:30 +0800

    Modify about section spacing

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,28 +2,27 @@ const About = () => {
   return (
     <div className="flex flex-col items-center h-auto gap-y-[40px] my-[80px] w-full">
       <h2 className="font-bold text-3xl">About</h2>
-      <div className="flex w-2/3">
-        <div className="w-full">
+      <div className="flex w-2/3 gap-x-[40px]">
+        <div className="w-full flex justify-center">
           <img
             src="/images/formal-picture.png"
             alt="formal picture"
             className="h-[320px] rounded-lg"
           />
         </div>
-        <div className="w-full">
+        <div className="w-full flex justify-center">
           <p>
             I am Christian Roed Boyles, a skilled Frontend Developer, Web
             Developer, and Programmer with expertise in HTML, CSS, JavaScript,
             and modern frameworks like React, Next.js, and Vue.js. I am also
             proficient in tools such as Git, GitHub, GitLab, Python, PHP,
             Selenium, Bitbucket, Atlassian, Jira, Azure, and AWS, I specialize
-            in responsive web design, mobile-first development, and
-            cross-browser compatibility. They excel in API integration, CI/CD,
-            and web application security while focusing on scalable solutions
-            and server-side rendering for improved SEO. With a proactive,
-            self-motivated work ethic, I consistently update my skills to
-            deliver cutting-edge digital solutions that align with business
-            goals.
+            in responsive web design and cross-browser compatibility. I excel
+            in API integration, CI/CD, and web application security while
+            focusing on scalable solutions and server-side rendering for
+            improved SEO. With a proactive, self-motivated work ethic, I
+            consistently update my skills to deliver cutting-edge digital
+            solutions that align with business goals.
           </p>
         </div>
       </div>
