d647f5384f4b3022d3a65dbaa6415ab28afdb6b1
rockyroed <roed-imp.vo97b@slmail.me>
2025-02-18 11:21:31 +0800

    Fix foreground className

--- src/components/Hero/Mouse.jsx
+++ src/components/Hero/Mouse.jsx
@@ -13,7 +13,7 @@ export const Mouse = () => {
   return (
     <div className="w-[24px] h-[40px] border-2 rounded-full flex justify-center items-start relative overflow-hidden">
       <div
-        className={`w-[6px] h-[6px] bg-text rounded-full absolute top-2 transition-transform duration-[1200ms] ease-in-out ${position ? 'translate-y-[10px]' : 'translate-y-0'}`}
+        className={`w-[6px] h-[6px] bg-foreground rounded-full absolute top-2 transition-transform duration-[1200ms] ease-in-out ${position ? 'translate-y-[10px]' : 'translate-y-0'}`}
       />
     </div>
   );
