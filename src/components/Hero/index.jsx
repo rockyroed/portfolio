4ceadee7ad89b1c21ac5881dc957f052a7513c29
rockyroed <roed-imp.vo97b@slmail.me>
2025-04-03 12:40:43 +0800

    Modify navbar and hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,19 +1,33 @@
-import { Mouse } from "@/components/Hero/Mouse";
+import { CTAButton } from "@/components/Button";
+import { FloatingHeading } from "@/components/Hero/Float";
 
 const Hero = () => {
   return (
-    <div className="pt-[160px] flex text-center flex-col justify-between items-center gap-y-[200px]">
-      <div className="flex flex-col justify-center items-center gap-y-[32px]">
-        <h1 className="text-5xl/16 font-bold w-2/3">
-          Paving the Web to Create Smooth Digital Journeys
-        </h1>
-        <p className="w-1/2">
-          I develop websites with a focus on functionality, responsiveness, and
-          user experience, navigating the challenges of web development to
-          create smoother digital experiences.
-        </p>
+    <div className="py-16 flex justify-between items-center">
+      <div className="flex flex-col gap-11 w-1/2 px-8">
+        <div className="flex flex-col gap-6">
+          <h1 className="text-h1 font-semibold text-text">
+            Building{" "}
+            <span className="text-foreground">High-Performance Websites</span>{" "}
+            for Seamless Digital Experiences
+          </h1>
+          <h6 className="text-text text-h6">
+            I build fast, responsive, and user-focused websites that transform
+            complex challenges into seamless, high-performing digital
+            experiences.
+          </h6>
+        </div>
+        <CTAButton value="Contact Me" />
       </div>
-      <Mouse />
+      <div className="w-1/2 flex justify-center">
+        <FloatingHeading value="Device Responsiveness" className="top-40 -left-40" />
+        <FloatingHeading value="Web Development" className="top-15 -left-20" />
+        <div className="pt-16">
+          <img src="/images/hero.png" alt="Hero Image" className="w-[35rem]" />
+        </div>
+        <FloatingHeading value="Frontend Development" className="top-15 -left-40" />
+        <FloatingHeading value="API Integration" className="top-40 -left-20" />
+      </div>
     </div>
   );
 };
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,9 +1,9 @@
-import Button from "@/components/Button";
+import { NavBarButton } from "@/components/Button";
 
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-between h-20 w-full">
-      <div className="flex justify-start items-center w-auto gap-x-1">
+    <div className="flex items-center justify-between h-20 w-full py-5">
+      <div className="flex justify-start items-center w-auto h-max gap-x-1">
         <img
           src="/images/logo-transparent.png"
           alt="logo"
@@ -11,15 +11,17 @@ const NavBar = () => {
           loading="eager"
           decoding="async"
         />
-        <h4 className="font-semibold font-jetbrains-mono text-h4 font-md">rockyroed</h4>
+        <h4 className="font-semibold font-jetbrains-mono text-h4 font-md">
+          rockyroed
+        </h4>
       </div>
       <div className="flex justify-end items-center gap-x-4 w-auto">
-        <Button value="About" />
-        <Button value="Stats" />
-        <Button value="Services" />
-        <Button value="Projects" />
-        <Button value="Certificates" />
-        <Button value="Contact" />
+        <NavBarButton value="About" />
+        <NavBarButton value="Stats" />
+        <NavBarButton value="Services" />
+        <NavBarButton value="Projects" />
+        <NavBarButton value="Certificates" />
+        <NavBarButton value="Contact" />
       </div>
     </div>
   );
