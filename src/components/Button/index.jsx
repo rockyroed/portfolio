be09020529106c35b78f845193fa83f16a54cc7e
rockyroed <roed-imp.vo97b@slmail.me>
2025-04-03 12:39:12 +0800

    Add proptypes to navbar button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Button = ({ value }) => {
+export const NavBarButton = ({ value }) => {
   return (
     <button className="cursor-pointer font-semibold text-text text-h5 px-4 py-1.5 hover:bg-background rounded-full">
       {value}
@@ -8,7 +8,7 @@ const Button = ({ value }) => {
   );
 };
 
-Button.propTypes = {
+NavBarButton.propTypes = {
   value: PropTypes.string.isRequired,
 };
 
