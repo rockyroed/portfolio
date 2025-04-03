5e3053eda3dbd14a8b5dba06d62974ea3cdb3e02
rockyroed <roed-imp.vo97b@slmail.me>
2025-04-03 12:49:06 +0800

    Comment about and stats section temporarily

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Stats from "@/components/Stats";
+// import About from "@/components/About";
+// import Stats from "@/components/Stats";
 
 const App = () => {
   return (
     <div className="flex flex-col justify-center items-center text-foreground mx-24">
       <NavBar />
       <Hero />
-      <About />
-      <Stats />
+      {/* <About />
+      <Stats /> */}
     </div>
   );
 };
