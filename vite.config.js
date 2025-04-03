ff57edc216f5b6c4b5e89ab312bb0ff6ac71ecc6
rockyroed <roed-imp.vo97b@slmail.me>
2025-04-03 13:00:37 +0800

    Setup github pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/portfolio",
   resolve: {
     alias: {
       "@": "/src",
